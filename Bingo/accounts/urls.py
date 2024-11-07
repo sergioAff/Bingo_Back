057c577d34c112b089c837c89f494e7a9776f8cb
Sergio Fernandez <sergioadrianfernandez02@gmail.com>
2024-11-07 15:31:28 -0300

    fix bugs

--- Bingo/accounts/urls.py
+++
@@ -1,8 +0,0 @@
-# from django.urls import path, re_path
-# from . import views
-
-# urlpatterns = [
-#     path('register/', views.register , name='register'),
-#     path('login/', views.login, name='login'),
-#     path('user/', views.user_profile, name='user-detail'),
-# ]
